f30a83d20ec96103b049a793ec83342144f494a5
meetVaidya <vaidyameet2004@gmail.com>
2024-07-02 08:11:46 +0530

    Refactor aggregate_data() function to accept bill_sheet and row parameters

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ month_file = file_import.month_file
 
 # Import aggregate here, after it's needed
 import aggregate
-aggregate.aggregate_data()
+bill_sheet = str(input("Enter the name of the bill sheet: "))
+row = int(input("Enter the row number: "))
+aggregate.aggregate_data(bill_sheet, row)
 
 time.sleep(2)
 
@@ -17,4 +19,5 @@ time.sleep(2)
 
 # Import print1 here, after it's needed
 import print1
-print1.process_excel_data()
+sheet_name = str(input("Enter the name of the sheet: "))
+print1.process_excel_data(sheet_name)

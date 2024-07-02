6edbae230a6fe2df73b929b59bc12c9170b3b865
meetVaidya <vaidyameet2004@gmail.com>
2024-07-02 08:12:35 +0530

    Update file paths for AutoExcelPDF inputs

--- file_import.py
+++ file_import.py
@@ -1,8 +1,8 @@
-excel_path = input("Enter the file path: ")
-# excel_path = r"D:\Projects\test\test.xlsm"
-month_file = input("Enter the month text file path: (if doesn't exist it will be created)")
-# month_file = r"D:\Projects\test\month.txt"
-bill_file = input("Enter the bill text file path: (if doesn't exist it will be created)")
-# bill_file = r"D:\Projects\test\bill_no.txt"
-company_file = input("Enter the company text file path: (if doesn't exist it will be created)")
-# company_file = r"D:\Projects\test\company_name.txt"
+# excel_path = input("Enter the file path: ")
+excel_path = r"D:\Projects\AutoExcelPDF\YJV_Invoice_New.xlsm"
+# month_file = input("Enter the month text file path: (if doesn't exist it will be created)")
+month_file = r"D:\Projects\AutoExcelPDF\month.txt"
+# bill_file = input("Enter the bill text file path: (if doesn't exist it will be created)")
+bill_file = r"D:\Projects\AutoExcelPDF\bill_no.txt"
+# company_file = input("Enter the company text file path: (if doesn't exist it will be created)")
+company_file = r"D:\Projects\AutoExcelPDF\company_name.txt"
